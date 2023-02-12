45f2f5dd7b5d7d053ac2cd37b09040be5894608f
xpy123993 <xpy123993@gmail.com>
2023-02-11 22:46:39 -0800

    Remove dns set up script.

--- conntable_windows.go
+++ conntable_windows.go
@@ -16,12 +16,10 @@ func redirectPipeExecute(name string, args ...string) error {
 }
 
 func PostTunnelSetup(localNet *netip.Prefix, devName, dnsSuffix string) error {
-	if err := redirectPipeExecute("netsh", "interface", "ip", "set", "address", "name="+devName, "source=static", "addr="+localNet.String(), "gateway=none"); err != nil {
+	if err := redirectPipeExecute("netsh", "interface", "ipv4", "set", "address", "name="+devName, "source=static", "addr="+localNet.String(), "gateway=none"); err != nil {
 		log.Printf("Configure tunnel failed.")
 	}
-	if err := redirectPipeExecute("netsh", "interface", "ipv4", "set", "dns", "name="+devName, "source=static", "addr="+localNet.Addr().String()); err != nil {
-		log.Printf("Configure DNS failed.")
-	}
+	log.Printf("Warning: DNS server is up on %s but dns split is not supported", localNet.Addr().String())
 	return nil
 }
 
